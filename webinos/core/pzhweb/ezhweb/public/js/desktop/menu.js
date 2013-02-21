70b123a1e71100729ec49090d4089bd76706d475
Toby Ealden <toby.ealden@gmail.com>
2013-02-21 10:01:34 +0000

    Added IOT dashboard

--- webinos/core/pzhweb/ezhweb/public/js/desktop/menu.js
+++ webinos/core/pzhweb/ezhweb/public/js/desktop/menu.js
@@ -14,4 +14,13 @@ $(function() {
 
     return false;
   });
+
+  $("#menu-iot-dashboard").click(function() {
+    ubi.console.log("Loading iot dashboard");
+    var url = "http://" + window.location.hostname + ":8080/apps/app-sem/index.html";
+    $(".ui-layout-center").html("");
+    $(".ui-layout-center").append("<iframe style='' scrolling='auto' width='100%' height='100%' frameborder='0' src='" + url + "'></iframe>");
+
+    return false;
+  });
 });
